10d4606125c9ec936ef3240e4f6b4c709535fd53
ducduong2002 <duongtd@zigvy.com>
2025-01-14 09:49:34 +0700

    Xóa tất cả file khỏi Git

--- client/src/store/reducer.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import userReducer from "./login/userSlice";
-
-const rootReducer = combineReducers({
-  user: userReducer,
-
-});
-
-export default rootReducer;
